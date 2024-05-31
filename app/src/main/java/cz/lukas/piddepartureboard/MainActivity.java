0409df529f8395f934902fed487797f2f34c8977
JanetLu97 <lukas.janetschek@email.cz>
2024-05-31 20:47:14 +0200

    Intial app setup with MainActivity and proper gradle version

---
+++ app/src/main/java/cz/lukas/piddepartureboard/MainActivity.java
@@ -0,0 +1,14 @@
+package cz.lukas.piddepartureboard;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
